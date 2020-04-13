33362c5ddbccdc67fcb4c7b87cb07350a6f411ee
Loki Astari <Loki.Astari@gmail.com>
2020-04-13 08:38:32 -0700

    Add all the basic template functions to file.cpp so we can run test on them and get coverage.

--- src/ThorsStorage/file.cpp
+++ src/ThorsStorage/file.cpp
@@ -8,6 +8,30 @@
 #include "file.tpp"
 #include "test/Person.h"
 
-template ThorsAnvil::FileSystem::ColumnFormat::FileBase<std::fstream, Person>::FileBase(std::string, unsigned int);
+using ThorsAnvil::FileSystem::ColumnFormat::Impl::FileReader;
+using ThorsAnvil::FileSystem::ColumnFormat::Impl::FileWriter;
+using ThorsAnvil::FileSystem::ColumnFormat::FileBase;
+
+template void FileReader<FileBase<std::fstream, Person>, Person>::read(FileBase<std::fstream, Person>&, Person&);
+template void FileReader<std::fstream, int>::read(std::fstream&, int&);
+template void FileReader<std::fstream, std::string>::read(std::fstream&, std::string&);
+
+template void FileWriter<FileBase<std::fstream, Person>, Person>::write(FileBase<std::fstream, Person>&, Person const&);
+template void FileWriter<std::fstream, int>::write(std::fstream&, int const&);
+template void FileWriter<std::fstream, std::string>::write(std::fstream&, std::string const&);
+
+template FileBase<std::fstream, Person>::FileBase(std::string, std::ios_base::openmode);
+
+template void FileBase<std::fstream, Person>::open(std::string, std::ios_base::openmode);
+template void FileBase<std::fstream, Person>::open(std::ios_base::openmode);
+template void FileBase<std::fstream, Person>::close();
+template void FileBase<std::fstream, Person>::read(Person&);
+template void FileBase<std::fstream, Person>::write(Person const&);
+
+template void FileBase<std::fstream, Person>::setstateSubFiles<0, 1>(iostate, std::index_sequence<0, 1>);
+template void FileBase<std::fstream, Person>::doCloseMembers<0, 1>(std::index_sequence<0, 1>);
+template void FileBase<std::fstream, Person>::doOpenMembers<0, 1>(std::ios_base::openmode, std::index_sequence<0, 1>);
+template void FileBase<std::fstream, Person>::writeMembers<0, 1>(Person const&, std::index_sequence<0, 1>);
+template void FileBase<std::fstream, Person>::readMembers<0, 1>(Person&, std::index_sequence<0, 1>);
 
 #endif
