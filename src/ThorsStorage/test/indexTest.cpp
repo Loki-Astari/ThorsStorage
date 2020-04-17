b18f15b6cbe4b8e67436d9a8b85a365ec188a4f0
Loki Astari <Loki.Astari@gmail.com>
2020-04-16 23:59:43 -0700

    Add a test for index into string column

---
+++ src/ThorsStorage/test/indexTest.cpp
@@ -0,0 +1,36 @@
+#include "gtest/gtest.h"
+
+#include "file.h"
+#include "test/Person.h"
+#include "test/TestFileClass.h"
+
+namespace TC=ThorsAnvil::FileSystem::ColumnFormat;
+
+using IndexTest = OpenTwoPeopleTest;
+
+TEST_F(IndexTest, indexFileCheck)
+{
+    {
+        TC::OFile<TwoPeople>    file(simpleTestDir);
+        file << TwoPeople{Person{"Martin" ,20}, Person{"Loki", 22}};
+        ASSERT_TRUE(file);
+    }
+    std::size_t     index;
+
+    std::fstream    index1(simpleTestP1NameI);
+    ASSERT_TRUE(index1);
+
+    index1.read(reinterpret_cast<char*>(&index), sizeof index);
+    ASSERT_TRUE(index1);
+
+    ASSERT_EQ(index, 7);    // Martin + '\n'
+
+
+    std::fstream    index2(simpleTestP1NameI);
+    ASSERT_TRUE(index2);
+
+    index2.read(reinterpret_cast<char*>(&index), sizeof index);
+    ASSERT_TRUE(index2);
+
+    ASSERT_EQ(index, 7);    // Loki + '\n'
+}
