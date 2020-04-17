50f75a8c0223f604ae6dcf9f2072227843ab5ee8
Loki Astari <Loki.Astari@gmail.com>
2020-04-16 22:40:25 -0700

    Add more tests

---
+++ src/ThorsStorage/test/closeTest.cpp
@@ -0,0 +1,40 @@
+#include "gtest/gtest.h"
+
+#include "file.h"
+#include "test/Person.h"
+#include "test/TestFileClass.h"
+
+namespace TC=ThorsAnvil::FileSystem::ColumnFormat;
+
+using CloseTest = OpenTwoPeopleTest;
+
+TEST_F(CloseTest, closeTwoPeopleFile)
+{
+    TC::File<TwoPeople>    file(simpleTestDir, std::ios::out);
+    ASSERT_TRUE(file);
+
+    file.close();
+    ASSERT_FALSE(file);
+}
+
+TEST_F(CloseTest, closeTwoPeopleOFile)
+{
+    TC::OFile<TwoPeople>    file(simpleTestDir);
+    ASSERT_TRUE(file);
+
+    file.close();
+    ASSERT_FALSE(file);
+}
+
+TEST_F(CloseTest, closeTwoPeopleIFile)
+{
+    {
+        TC::OFile<TwoPeople>    file(simpleTestDir);
+    }
+    TC::IFile<TwoPeople>    file(simpleTestDir);
+    ASSERT_TRUE(file);
+
+    file.close();
+    ASSERT_FALSE(file);
+}
+
---
+++ src/ThorsStorage/test/stringTest.cpp
@@ -0,0 +1,46 @@
+#include "gtest/gtest.h"
+
+#include "file.h"
+#include "test/Person.h"
+#include "test/TestFileClass.h"
+
+namespace TC=ThorsAnvil::FileSystem::ColumnFormat;
+
+using StringTest = OpenTwoPeopleTest;
+
+TEST_F(StringTest, WriteMultoLineString)
+{
+    {
+        TC::OFile<TwoPeople>    file(simpleTestDir);
+        ASSERT_TRUE(file);
+        file << TwoPeople{Person{"Mult-Line\nString\n1234" ,20}, Person{"Loki", 22}};
+        ASSERT_TRUE(file);
+    }
+
+    std::string     line;
+    int             number;
+
+    std::ifstream   p1nameFile(simpleTestP1Name);
+    std::getline(p1nameFile, line);
+    ASSERT_EQ(line, std::string("Mult-Line\000String\0001234", 21));
+    ASSERT_TRUE(p1nameFile);
+}
+
+TEST_F(StringTest, ReadMultoLineString)
+{
+    {
+        TC::OFile<TwoPeople>    file(simpleTestDir);
+        ASSERT_TRUE(file);
+        file << TwoPeople{Person{"Mult-Line\nString\n1234" ,20}, Person{"Loki", 22}};
+        ASSERT_TRUE(file);
+    }
+
+    TC::IFile<TwoPeople>    file(simpleTestDir);
+    TwoPeople               twoPerson;
+
+    file >> twoPerson;
+    ASSERT_TRUE(file);
+
+    ASSERT_EQ(twoPerson.personOne.name, "Mult-Line\nString\n1234");
+}
+
